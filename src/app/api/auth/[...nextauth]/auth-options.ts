9575988d81edc667e2973044f2c3630b231bb7fb
Verner Codoceo <vacodoceo@uc.cl>
2023-06-01 11:02:51 -0400

    🛂 add basic github provider oauth config

--- src/app/api/auth/[...nextauth]/auth-options.ts
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -6,6 +6,24 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      authorization: {
+        url: "https://github.com/login/oauth/authorize",
+        params: { scope: "read:project" },
+      },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // @ts-ignore
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/auth-options";
 import { redirect } from "next/navigation";
+import { SignOutButton } from "@/components/buttons/sign-out-button";
 
 const Home = async () => {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/api/auth/signin");
 
+  console.log({ session });
+
   return (
     <main className="flex flex-col items-center p-8 space-y-8">
       PR Label Visualizer
+      <SignOutButton />
     </main>
   );
 };
